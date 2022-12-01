3175569e6b736ea8294e0387577926eba728cec7
Chris Farley <cfarley95@gmail.com>
2022-11-30 23:19:39 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, request, jsonify
+
+import psycopg2
+import psycopg2.extras
+import psycopg2.extensions
+import logging
+import sys
+import json
+import requests
+import os
+
+app = Flask(__name__)
+
+app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
+
+
+@app.route('/',methods=['GET', 'POST'])
+def hello_world():  # put application's code here
+    data = request.json
+
+    connection = psycopg2.connect(app.config["DATABASE_URL"])
+    dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    try:
+        with dict_cur as cursor:
+            sql = '''SELECT * FROM buyer
+                      '''
+
+
+
+            cursor.execute(sql,)
+
+
+            result = cursor.fetchall()
+            print(result)
+
+            connection.commit()
+    finally:
+        connection.close()
+    return render_template("index.html", data=jsonify(result))
+
+
+@app.route('/items', methods=['GET', 'POST'])
+def show_items():  # put application's code here
+    data = request.json
+
+    connection = psycopg2.connect(app.config["DATABASE_URL"])
+    dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    try:
+        with dict_cur as cursor:
+            sql = '''SELECT * FROM item
+                      '''
+
+            cursor.execute(sql, )
+
+            result = cursor.fetchall()
+            print(result)
+
+            connection.commit()
+    finally:
+        connection.close()
+    return render_template("items.html", data=jsonify(result))
+
+@app.route('/bids', methods=['GET', 'POST'])
+def show_bids():  # put application's code here
+    data = request.json
+
+    connection = psycopg2.connect(app.config["DATABASE_URL"])
+    dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    try:
+        with dict_cur as cursor:
+            sql = '''SELECT * FROM bid
+                      '''
+
+            cursor.execute(sql, )
+
+            result = cursor.fetchall()
+            print(result)
+
+            connection.commit()
+    finally:
+        connection.close()
+    return render_template("bids.html", data=jsonify(result))
+
+
+if __name__ == '__main__':
+    app.run()
